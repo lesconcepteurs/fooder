31531a411d23ae8d2120f6c9f3106e46dc65bf02
Florian <f.guillemet@live.fr>
2019-03-04 16:25:14 +0100

    creation servlet controleurFacade.java Etat: vide

---
+++ src/main/java/les/concepteurs/fooder/controller/ControleurFacade.java
@@ -0,0 +1,16 @@
+package les.concepteurs.fooder.controller;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet qui sert de controleur de façade.
+ * @author stag05
+ *
+ */
+public class ControleurFacade extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
