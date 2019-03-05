fde397854b5b043a4ef419fc340cba616b393ea4
Florian <f.guillemet@live.fr>
2019-03-05 15:04:47 +0100

    classe ListePreparationsDAO.java complète avec requête

--- src/main/java/les/concepteurs/fooder/implement/ListePreparationsDAO.java
+++ src/main/java/les/concepteurs/fooder/implement/ListePreparationsDAO.java
@@ -2,9 +2,8 @@ package les.concepteurs.fooder.implement;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-
 import les.concepteurs.fooder.dao.DAO;
 import les.concepteurs.fooder.metier.ListePreparations;
 import les.concepteurs.fooder.metier.Preparation;
@@ -19,11 +18,18 @@ import les.concepteurs.fooder.metier.Preparation;
 public class ListePreparationsDAO extends DAO<ListePreparations> {
 	
 	PreparedStatement prepare = null;
+	//nom des champs dans les tables 
+	private final String 	NOMPREPA 	= "nom_type_prepa";
+	private final String	DUREE 		= "duree";
+	private final String 	NOMUNITE 	= "nom_unite_duree";
+	private final String	DUREESECOND = "duree_second";
+	
 	
 	public ListePreparationsDAO(Connection conn) {
 		super(conn);		
 	}
 
+	
 	@Override
 	public boolean create(ListePreparations obj) {
 		// TODO Auto-generated method stub
@@ -43,26 +49,53 @@ public class ListePreparationsDAO extends DAO<ListePreparations> {
 	}
 
 	/**
-	 * Methode de recherche en base des données concernant les préparation de recettes.
+	 * Methode de recherche en base des données concernant les préparations de recettes.
 	 * puis crée les objets Preparation.
 	 * puis les ajoute à ListePreparation (ArrayList) 
 	 */
+	@SuppressWarnings("null")
 	@Override
 	public ListePreparations find(int id) throws SQLException {
 		
 		ListePreparations listePreparations = null;
 		
-		String requetePrepare = "";
+		String requetePrepare = "select sp.id_rec, tp.nom_type_prepa, sp.duree, ud.nom_unite_duree, sp.duree_second \r\n" + 
+				"from se_prepare sp \r\n" + 
+				"left join type_prepa tp on sp.id_type_prepa = tp.id_type_prepa\r\n" + 
+				"left join unite_duree ud on sp.id_unite_duree = ud.id_unite_duree\r\n" + 
+				"where id_rec=?;";
 		
 		try {
 			
 			prepare = connect.prepareStatement(requetePrepare);
 			
 			//Set du ? par l'id envoyé qui se trouve à la position 1 de la table se_prepare
-			prepare.setInt(1, id);;
+			prepare.setInt(1, id);
 			
-		}catch (Exception e) {
-			e.printStackTrace();
+			ResultSet result = prepare.executeQuery();
+			
+//			if (result == null) throw new ResultSetNull();*/
+//			else {
+//								
+				while (result.next()) {
+					
+					//Creation d'un objet Preparation.			
+					Preparation preparation = new Preparation(
+							result.getString(NOMPREPA), 
+							result.getInt(DUREE), 
+							result.getString(NOMUNITE), 
+							result.getInt(DUREESECOND)
+							);
+					
+					//ajout dans la listePreparation
+					listePreparations.add(preparation);
+				}
+						
+//			}			
+			
+		}catch (Exception e ) {
+			e.printStackTrace();			
+			System.out.println(e.getMessage());
 			
 		}finally {
 			if (prepare != null)	prepare.close();
@@ -71,8 +104,5 @@ public class ListePreparationsDAO extends DAO<ListePreparations> {
 		
 		return listePreparations;
 	}
-
-
-	
 
 }
