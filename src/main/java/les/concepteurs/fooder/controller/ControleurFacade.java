2d5d15f725e6c6fc134836353f7966f2fdf1f8df
Florian <f.guillemet@live.fr>
2019-03-05 08:57:29 +0100

    suppression des accents dans les commentaires du controleurFacade.java

--- src/main/java/les/concepteurs/fooder/controller/ControleurFacade.java
+++ src/main/java/les/concepteurs/fooder/controller/ControleurFacade.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet qui sert de controleur de facade et redispatche sur d'autres servlet de fonctionnalité.
+ * Servlet qui sert de controleur de facade et redispatche sur d'autres servlet de fonctionnalite.
  * @author stag05
  * 
  */
@@ -27,15 +27,16 @@ public class ControleurFacade extends HttpServlet{
 	private RequestDispatcher disp;
 	
 	/**
-	 * Methode qui va recupérer l'url (à partir du chemin relatif à la servlet, cad le chemin après l'urlPattern de la servlet)
+	 * Methode qui va recuperer l'url (A� partir du chemin relatif A� la servlet, 
+	 * cad le chemin apres l'urlPattern de la servlet)
 	 * et rediriger vers une autre servlet en fonction du path.
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String path = request.getPathInfo();
 		
-		System.out.println("Je suis dans le Controleur de façade. Path= "+path);
-		System.out.println("Je suis dans le Controleur de façade. Contexte path= "+ request.getContextPath());
+		System.out.println("Je suis dans le Controleur de facade. Path= "+path);
+		System.out.println("Je suis dans le Controleur de facade. Contexte path= "+ request.getContextPath());
 		
 		if (path == null || path.equals("/"))		doAccueil(request, response);
 		else if (path.equals("/listeRecettes"))		doListeRecettes(request, response);
@@ -47,7 +48,7 @@ public class ControleurFacade extends HttpServlet{
 
 
 	/**
-	 * Méthode qui renvoie vers la page index.jsp
+	 * Methode qui renvoie vers la page index.jsp
 	 * @param request
 	 * @param response
 	 * @throws ServletException
@@ -60,7 +61,7 @@ public class ControleurFacade extends HttpServlet{
 	}
 	
 	/**
-	 * Méthode qui renvoie vers la page de liste des recettes
+	 * Methode qui renvoie vers la page de liste des recettes
 	 * @param request
 	 * @param response
 	 * @throws IOException 
