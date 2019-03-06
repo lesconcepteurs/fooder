afae0c89f43226a77f34966efa6c7c07207ebb22
Philippe <philippe@pcohen.fr>
2019-03-06 18:05:28 +0100

    Cleaning unused class testServlet

--- src/main/java/les/concepteurs/fooder/app/ListeRecettes.java
+++ src/main/java/les/concepteurs/fooder/app/ListeRecettes.java
@@ -1,10 +1,21 @@
 package les.concepteurs.fooder.app;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import les.concepteurs.fooder.dao.DAOConnect;
+import les.concepteurs.fooder.implement.IngredientDAO;
+import les.concepteurs.fooder.implement.RecetteDAO;
+import les.concepteurs.fooder.metier.Ingredient;
+import les.concepteurs.fooder.metier.Recette;
 
 /**
  * Servlet implementation class ListeRecettes
@@ -25,7 +36,37 @@ public class ListeRecettes extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+	    response.setCharacterEncoding( "UTF-8" );
+	    PrintWriter out = response.getWriter();
+		methodeTest(out);
+		
+		
+	}
+
+	private void methodeTest(PrintWriter out) {
+		Ingredient ing = null;
+		
+		try {
+			 ing = new IngredientDAO(DAOConnect.getConnexion()).find(3);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		out.println("<!DOCTYPE html>");
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<meta charset=\"utf-8\" />");
+	    out.println("<title>Test</title>");
+	    out.println("</head>");
+	    out.println("<body>");
+	    out.println("<p>Ingredient (toString) : "+ing+"</p>");
+	    out.println("</body>");
+	    out.println("</html>");
+		
 	}
 
 	/**
